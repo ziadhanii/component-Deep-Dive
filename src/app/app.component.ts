9f1de560df0f5e93c1042f515437becd60221349
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-02-18 22:46:04 +0200

    Add initial assets and styles; implement basic app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'component-Deep-Dive';
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
